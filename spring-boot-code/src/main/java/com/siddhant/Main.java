110008f496870ede9716b40c12f7c82441185432
sidmercer <siddhantk1247@gmail.com>
2026-01-01 15:21:40 +0530

    Initial commit

---
+++ spring-boot-code/src/main/java/com/siddhant/Main.java
@@ -0,0 +1,20 @@
+package com.siddhant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args){
+//        System.out.println("Hello, SpringBoot started!!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String Greet(){
+       return "Hello this is the first API";
+    }
+}
