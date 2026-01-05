ba5914f9c173ff233dbbcccf8a940f1d6a1ca054
sidmercer <siddhantk1247@gmail.com>
2026-01-05 23:52:53 +0530

    added dto

---
+++ spring-boot-code/src/main/java/com/siddhant/dto/GreetResponse.java
@@ -0,0 +1,20 @@
+package com.siddhant.dto;
+
+public class GreetResponse {
+
+    private String message;
+    private int status;
+
+    public GreetResponse(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
